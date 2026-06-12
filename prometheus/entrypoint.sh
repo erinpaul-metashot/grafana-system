a888aa6ee9f1e4abbf31880f4715695ca127f136
erinpaul-metashot <erinpaul@metashot.co.in>
2026-06-12 11:11:22 +0530

    fixing prom env issue

---
+++ prometheus/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Replace the variables with the environment variables (with defaults applied from Dockerfile ENV)
+sed -i "s/\${BACKEND_TARGET}/$BACKEND_TARGET/g" /etc/prometheus/prometheus.yml
+sed -i "s/\${BACKEND_JOB_NAME}/$BACKEND_JOB_NAME/g" /etc/prometheus/prometheus.yml
+
+# Execute the Prometheus binary with all arguments
+exec /bin/prometheus \
+      --config.file=/etc/prometheus/prometheus.yml \
+      --storage.tsdb.path=/prometheus \
+      --storage.tsdb.retention.time=365d \
+      --web.console.libraries=/usr/share/prometheus/console_libraries \
+      --web.console.templates=/usr/share/prometheus/consoles \
+      --web.external-url=http://localhost:9090 \
+      --log.level=info
